c46d663445034d244a006c614a60c5633931a7b9
Ryan LeViseur <vidkun@vlfservices.com>
2013-12-21 14:09:53 -0600

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Included for Capybara/RSpec tests
+  config.include Capybara::DSL
 end
